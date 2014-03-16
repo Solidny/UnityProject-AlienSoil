9fb90c4a2c113c8b7a21195e6c9b573266535a38
Solidny <solidnypan@gmail.com>
2014-03-16 22:25:24 +0000

    update_007_TPPcamera

---
+++ Assets/CharacterControllerLogic.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterControllerLogic : MonoBehaviour 
+{
+	
+	[SerializeField]
+	private Animator animator;
+	[SerializeField]
+	private float DirectionDampTime = .25f;
+	private float speed = 0.0f;
+	private float h = 0.0f;
+	private float v = 0.0f;
+	
+		
+		
+	// Use this for initialization
+	void Start () 
+	{	
+		animator = GetComponent<Animator>();
+		
+		if (animator.layerCount >= 2)
+		{
+			animator.SetLayerWeight(1, 1);
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (animator)
+		{
+			h = Input.GetAxis("Horizontal");
+			v = Input.GetAxis ("Vertical");
+			speed = new Vector2 (h, v).sqrMagnitude;
+			
+			animator.SetFloat("Speed", speed);
+			if (Input.GetKey(KeyCode.LeftShift))
+			{
+				animator.SetFloat ("WalkSpeed", 1, DirectionDampTime, Time.deltaTime);	
+			}
+			else 
+			{
+				animator.SetFloat ("WalkSpeed", 0, DirectionDampTime, Time.deltaTime);	
+			}
+		}
+	}
+}
